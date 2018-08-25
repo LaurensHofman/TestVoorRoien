6b88bb22b51ce3396e0403ce68643362af092d0f
laure <laure@DESKTOP-PKK44LS>
2018-08-26 00:15:18 +0200

    dag roien

--- TestVoorRoien/MainWindow.xaml.cs
+++ TestVoorRoien/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,74 @@ namespace TestVoorRoien
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+            grd.DataContext = this;           
+
+            TestList = new ObservableCollection<TestObjects>(new List<TestObjects>()
+            {
+                new TestObjects()
+                {
+                    Text = "Persoon 1"
+                },
+                new TestObjects()
+                {
+                    Text = "Persoon 2"
+                }
+            });
+
+            TestDropdownValues = new List<TestDropdownValues>()
+            {
+                new TestDropdownValues()
+                {
+                    Value = "Laurens Hofman",
+                    Display = "De Haan"
+                },
+                new TestDropdownValues()
+                {
+                    Value = "Roien Eshaghzey",
+                    Display = "Bredene"
+                }
+            };
+
+            dg.ItemsSource = TestList;
+            dg.DataContext = TestList;
         }
+
+        public List<TestDropdownValues> TestDropdownValues { get; set; }
+
+        public ObservableCollection<TestObjects> TestList { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dg_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            // Hier kan je dan een update/save uitvoeren
+        }
+    }
+
+    public class TestObjects
+    {
+        public string Text { get; set; }
+
+        public string ListValue { get; set; }
+
+        public bool IsLaurens
+        {
+            get
+            {
+                return this.ListValue == "Laurens Hofman";
+            }
+        }
+    }
+
+    public class TestDropdownValues
+    {
+        public string Value { get; set; }
+
+        public string Display { get; set; }
     }
 }
